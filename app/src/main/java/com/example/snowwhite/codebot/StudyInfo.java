6e4b2ce07ae5899c356ffe002e7abbb2ff91146d
SnowDeveloper <contact.madhushreesingh@gmail.com>
2017-01-31 13:59:03 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/snowwhite/codebot/StudyInfo.java
@@ -0,0 +1,13 @@
+package com.example.snowwhite.codebot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudyInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_info);
+    }
+}
