c3d0fc287bd37a29b278176ba8b4f0108f691e7c
SnowDeveloper <contact.madhushreesingh@gmail.com>
2017-01-31 15:45:52 +0200

    changes

--- app/src/main/java/com/example/snowwhite/codebot/StudyInfo.java
+++ app/src/main/java/com/example/snowwhite/codebot/StudyInfo.java
@@ -1,9 +1,10 @@
 package com.example.snowwhite.codebot;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
-public class StudyInfo extends AppCompatActivity {
+public class StudyInfo extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
