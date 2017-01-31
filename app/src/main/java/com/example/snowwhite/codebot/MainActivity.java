0b3294a53388c2451ff1638c88cc798d29c960ce
SnowDeveloper <contact.madhushreesingh@gmail.com>
2017-01-31 15:07:44 +0200

    Adding Tutorial Page flow

--- app/src/main/java/com/example/snowwhite/codebot/MainActivity.java
+++ app/src/main/java/com/example/snowwhite/codebot/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.snowwhite.codebot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Main Page","Here");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startLearning(View view) {
+        Log.d("StudyInfo Page","Here");
+
+        Intent intent = new Intent(this, StudyInfo.class);
+        //startActivityForResult(intent, 1);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/snowwhite/codebot/PractiseBot.java
+++ app/src/main/java/com/example/snowwhite/codebot/PractiseBot.java
@@ -1,17 +1,13 @@
 package com.example.snowwhite.codebot;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class StudyInfo extends AppCompatActivity {
+public class PractiseBot extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_study_info);
-
-
+        setContentView(R.layout.activity_practise_bot);
     }
-
-
 }
