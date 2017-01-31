5b4cc1c45b4c39bdd6c3ea62878ec801babce21e
SnowDeveloper <contact.madhushreesingh@gmail.com>
2017-01-31 17:11:28 +0200

    Adding Study texts

--- app/src/main/java/com/example/snowwhite/codebot/StudyInfo.java
+++ app/src/main/java/com/example/snowwhite/codebot/StudyInfo.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class StudyInfo extends FragmentActivity {
 
@@ -13,6 +14,17 @@ public class StudyInfo extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_info);
+
+        TextView textView = (TextView) findViewById(R.id.studyTextView);
+        textView.setText("The easiest way to explain what coding is, is to say that coding is " +
+                "typing in the code. \n\nCoding is like writing, but in a specific computer language. " +
+                "Programming means writing up your own code. It is like writing in foreign language." +
+                "\n\nA programmer (or developer) is a person who writes a code. \n\nA programmer is like " +
+                "a writer, who writes a book. \n\nPrograms are lines of code that is understandable " +
+                "for computers. So programs are books written in a computer language and computer " +
+                "is the reader of this code. ");
+
+
     }
 
     public void startPractiseBot(View view) {
