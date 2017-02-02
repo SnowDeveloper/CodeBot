ada8f7239f4f944d2370f1262f1a44e5984e3015
ProditorMagnus <public.ravana@gmail.com>
2017-02-02 12:56:04 +0200

    Use himanshu-soni/ChatMessageView

---
+++ app/src/main/java/com/example/snowwhite/codebot/ChatMessageAdapter.java
@@ -0,0 +1,90 @@
+package com.example.snowwhite.codebot;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by himanshusoni on 06/09/15.
+ *
+ */
+public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.MessageHolder> {
+    private static final int MY_MESSAGE = 0, OTHER_MESSAGE = 1;
+
+    private List<ChatMessage> mMessages;
+    private Context mContext;
+
+    public ChatMessageAdapter(Context context, List<ChatMessage> data) {
+        mContext = context;
+        mMessages = data;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessages == null ? 0 : mMessages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        ChatMessage item = mMessages.get(position);
+
+        if (item.isMine()) return MY_MESSAGE;
+        else return OTHER_MESSAGE;
+    }
+
+    @Override
+    public MessageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == MY_MESSAGE) {
+            return new MessageHolder(LayoutInflater.from(mContext).inflate(R.layout.item_mine_message, parent, false));
+        } else {
+            return new MessageHolder(LayoutInflater.from(mContext).inflate(R.layout.item_other_message, parent, false));
+        }
+    }
+
+    public void add(ChatMessage message) {
+        mMessages.add(message);
+        notifyItemInserted(mMessages.size() - 1);
+    }
+
+    @Override
+    public void onBindViewHolder(MessageHolder holder, int position) {
+        ChatMessage chatMessage = mMessages.get(position);
+        if (chatMessage.isImage()) {
+            holder.ivImage.setVisibility(View.VISIBLE);
+            holder.tvMessage.setVisibility(View.GONE);
+
+            holder.ivImage.setImageResource(R.drawable.code_bot);
+        } else {
+            holder.ivImage.setVisibility(View.GONE);
+            holder.tvMessage.setVisibility(View.VISIBLE);
+
+            holder.tvMessage.setText(chatMessage.getContent());
+        }
+
+        String date = new SimpleDateFormat("hh:mm aa", Locale.getDefault()).format(new Date());
+        holder.tvTime.setText(date);
+
+
+    }
+
+    class MessageHolder extends RecyclerView.ViewHolder {
+        TextView tvMessage, tvTime;
+        ImageView ivImage;
+
+        MessageHolder(View itemView) {
+            super(itemView);
+            tvMessage = (TextView) itemView.findViewById(R.id.tv_message);
+            tvTime = (TextView) itemView.findViewById(R.id.tv_time);
+            ivImage = (ImageView) itemView.findViewById(R.id.iv_image);
+        }
+    }
+}
--- app/src/main/java/com/example/snowwhite/codebot/PractiseBot.java
+++ app/src/main/java/com/example/snowwhite/codebot/PractiseBot.java
@@ -2,14 +2,14 @@ package com.example.snowwhite.codebot;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
@@ -19,6 +19,7 @@ import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PractiseBot extends FragmentActivity {
@@ -26,9 +27,12 @@ public class PractiseBot extends FragmentActivity {
     private final Random random = new Random();
     private final String WIT_TOKEN = "EIYRHFKXVNNJHIV2GR4WDLW3FOAGJTRL";
 
-    private EditText editText;
-    private ViewGroup messageHolder;
-    private ImageView myMsgBtn;
+    private RecyclerView mRecyclerView;
+    private Button mButtonSend;
+    private EditText mEditTextMessage;
+    private ImageView mImageView;
+
+    private ChatMessageAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +41,40 @@ public class PractiseBot extends FragmentActivity {
 
         AndroidNetworking.initialize(getApplicationContext());
 
-        editText = (EditText) findViewById(R.id.userInput);
-        messageHolder = (ViewGroup) findViewById(R.id.message_holder);
-        myMsgBtn = (ImageView) findViewById(R.id.sendUserInput);
-        myMsgBtn.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mButtonSend = (Button) findViewById(R.id.btn_send);
+        mEditTextMessage = (EditText) findViewById(R.id.et_message);
+        mImageView = (ImageView) findViewById(R.id.iv_image);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
+        mRecyclerView.setAdapter(mAdapter);
+
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Perform action on click
-                displayOwnMessage(editText.getText().toString());
-                chatToWit(editText.getText().toString());
-                Log.d(TAG, String.format("sendUserInput: %s", editText.getText().toString()));
+                String message = mEditTextMessage.getText().toString();
+                if (TextUtils.isEmpty(message)) {
+                    return;
+                }
+                sendMessage(message);
+                mEditTextMessage.setText("");
+            }
+        });
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage("info");
             }
         });
     }
 
-    private void displayBotMessage(String message) {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.bot_responses, messageHolder, false);
-
-        TextView child = (TextView) layout.getChildAt(2);
-        child.setText(message);
-        messageHolder.addView(layout);
-    }
-
-    private void displayOwnMessage(String message) {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.user_responses, messageHolder, false);
-
-        TextView child = (TextView) layout.getChildAt(0);
-        child.setText(message);
-        messageHolder.addView(layout);
+    private void displayMessage(String message, boolean ownMessage) {
+        ChatMessage chatMessage = new ChatMessage(message, ownMessage, false);
+        mAdapter.add(chatMessage);
+        mRecyclerView.scrollToPosition(mAdapter.getItemCount() - 1);
     }
 
     private void chatToWit(String input) {
@@ -84,7 +92,7 @@ public class PractiseBot extends FragmentActivity {
                         // do anything with response
                         try {
                             Log.d(TAG, "onResponse message: " + response.getString("msg"));
-                            displayBotMessage(response.getString("msg"));
+                            displayMessage(response.getString("msg"), false);
                         } catch (JSONException e) {
                             Log.d(TAG, String.format("onResponse: %s", response));
                             Log.e(TAG, "onResponse: ", e);
@@ -97,6 +105,18 @@ public class PractiseBot extends FragmentActivity {
                         Log.d(TAG, String.format("onError: %s", error));
                     }
                 });
+    }
+
+    private void sendMessage(String message) {
+        chatToWit(message);
+        displayMessage(message, true);
+    }
+
+    private void sendMessage() {
+        ChatMessage chatMessage = new ChatMessage(null, true, true);
+        mAdapter.add(chatMessage);
+
+//        mimicOtherMessage();
     }
 
 }
